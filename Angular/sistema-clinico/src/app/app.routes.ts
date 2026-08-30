f7ffebd10751760948405efff941369a82718306
MikeWazowski01 <garcia.toledo.fabian@gmail.com>
2026-08-30 12:06:03 -0600

    Interfaz del menu de sistema clinico, carpeta de SCRIPT_BaseDatos el script para crear una tabla

--- Angular/sistema-clinico/src/app/app.routes.ts
+++ Angular/sistema-clinico/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Pacientes } from './pages/pacientes/pacientes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home', component: Home
+  },
+  {
+    path: 'pacientes', component: Pacientes
+  },
+  {
+    path: '', component: Home
+  }
+];
--- Angular/sistema-clinico/src/app/shared/navbar/navbar.ts
+++ Angular/sistema-clinico/src/app/shared/navbar/navbar.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
